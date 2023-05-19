873a841127bc785c27c29bd77deb04d3940d3975
larrymiami <larrymiami94@gmail.com>
2023-05-19 15:50:37 +0300

    add tailwind theming

---
+++ config/site.ts
@@ -0,0 +1,24 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Zing Afrique",
+  description:
+    "Zing-Afrique is a website for a restaurant that offers dine-in services, online ordering and table reservations.",
+  authors: [
+    {
+      name: "Larry Miami",
+    },
+  ],
+  generator:
+    "Zing Afrique, restaurant website, restaurant reservation, react, nextjs, tailwindcss",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/miamilarry78",
+    github: "https://github.com/larrymiami/Zing-Afrique",
+  },
+}
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,4 +3,4 @@ import localFont from "next/font/local"
 export const hauora = localFont({
   src: "../app/HauoraGX.ttf",
   variable: "--font-hauora",
-});
+})
